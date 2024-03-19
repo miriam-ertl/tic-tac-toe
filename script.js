fac2a30d7d051aa4edb9f95e428fa897a81d4723
MErtl <m.ertl@steinberg.de>
2024-03-19 18:08:47 +0100

    feat: create game and interaction logic

---
+++ script.js
@@ -0,0 +1,34 @@
+let currentPlayer = "X";
+let gameBoard = ["", "", "", "", "", "", "", "", ""];
+let gameActive = true;
+
+function handlePlayerTurn(clickedCellIndex) {
+  if (gameBoard[clickedCellIndex] !== "" || !gameActive) {
+    return;
+  }
+  gameBoard[clickedCellIndex] = currentPlayer;
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
+
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", cellClicked, false);
+});
+
+function cellClicked(clickedCellEvent) {
+  const clickedCell = clickedCellEvent.target;
+  const clickedCellIndex = parseInt(clickedCell.id.replace("cell-", "")) - 1;
+
+  if (gameBoard[clickedCellIndex] !== "" || !gameActive) {
+    return;
+  }
+  handlePlayerTurn(clickedCellIndex);
+  updateUI();
+}
+
+function updateUI() {
+  for (let i = 0; i < cells.length, i++; ) {
+    cells[i].innerText = gameBoard[i];
+  }
+}
